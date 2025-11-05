ff9c7e985f6c56b5541e3d2aecbf37104f4ee65a
CN010627 <guanxing.zhu@sdlg.com>
2025-11-05 16:42:12 +0800

    update:

--- flake.nix
+++ flake.nix
@@ -19,11 +19,7 @@
       {
         nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
           specialArgs = { 
-            inherit system;
-            inherit inputs;
-            pkgs
-            username
-            hostname 
+            inherit system inputs pkgs username hostname 
           };
           system = "x86_64-linux";
           modules = [
