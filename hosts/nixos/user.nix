426c029ae5bd1414c50ac23546656eab6ad5200b
CN010627 <guanxing.zhu@sdlg.com>
2025-11-05 17:48:57 +0800

    u

--- hosts/nixos/user.nix
+++ hosts/nixos/user.nix
@@ -1,6 +1,10 @@
 { config, pkgs, username,... }:
+let
+  inherit (import ./variables.nix) gitUsername shell;
+in
 {
   users = {
+    mutableUsers = true;
     users."${username}" = {
       homeMode = "755";
       isNormalUser = true;
@@ -8,11 +12,110 @@
       extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
       initialPassword = "lumos";
     };
-
-    # 允许过期不维护的包
-    nixpkgs.config.permittedInsecurePackages = [
-      "electron-11.5.0" # NUR baidunetdisk needed
-    ];
-
   }
+  # 允许过期不维护的包
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-11.5.0" # NUR baidunetdisk needed
+  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      # software
+      # translate-shell # 命令行翻译
+      # starship
+      # stow # dotfiles needed
+      # tuckr # better than stow
+      # libcaca # img2txt
+      # tectonic-unwrapped # TeX/LaTeX 公式渲染
+      # nixfmt-rfc-style # 官方 nixfmt 风格
+      # nixd # Nix lsp
+      # gh # github cli
+      # jujutsu # better than git
+      # aircrack-ng # wifi hack
+      # socat # ipc
+      # yq # yaml 文件解析
+      # bintools
+      # udiskie # auto mount
+      # gpu-screen-recorder # record video
+      # satty # Screenshot Annotation
+
+      lazygit
+      # bottom
+      # neovim
+      yazi
+      # rmpc # music cli
+      # rsclock # colock
+      # asciinema # rec demo.cast
+      # asciinema-agg # cast -> gif
+      fuzzel
+      # networkmanager_dmenu
+      # posting # postman tui
+      # isd # systemd TUI
+      # kmon # 内核编译和管理TUI
+
+      # qbittorrent-enhanced # qbee
+      # motrix
+      # localsend
+      # ghostty
+      # foot
+      # rio
+      # zed-editor
+      # telegram-desktop
+      # stable.rustdesk-flutter
+      # evil-helix_git # introduces Vim keybindings and more
+      # element-desktop
+      # libreoffice-still # 长久支持版本
+      # foliate # epub reader
+      # bitwarden-desktop # 密码管理器
+      # kazumi # 番剧
+      # turntable # 音乐盒子
+      # librecad # CAD
+      # # (dbeaver-bin.override { override_xmx = "1024m"; }) # 数据库管理
+      # nautilus # file manager
+      # door-knocker # protal check
+
+      # # electron wrapper
+      # obsidian-wrapper
+      # vscodium-wrapper
+      # code-cursor-wrapper
+
+      # # cli tool
+      # neo-cowsay # fortune | cowsay --random --rainbow
+      # fortune
+      # pipes # grep
+      # sl
+      # figlet # ascii <font>
+      # bb
+      # cmatrix
+      # hollywood
+      # lolcat
+      # nitch
+      # xorg.xeyes
+      # ascii-image-converter
+    ]
+    # ++ [
+    #   # 自定义软件包
+    #   (pkgs.callPackage ../../pkgs/shimeji/package.nix { })
+    #   (pkgs.callPackage ../../pkgs/nsearch-tv.nix { })
+    # ]
+    # ++ (with pkgs.nur.repos.lonerOrz; [
+    #   # NUR packages
+    #   mpv-handler
+    #   go-musicfox
+    #   pear-desktop # youtube music
+    #   gitfetch
+    # ]);
+
+  programs = {
+    # 在此添加缺失的动态库（这些库会对未打包的程序生效）
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        glibc
+        icu
+      ];
+    };
+    ${shell}.enable = true;
+    starship.enable = true;
+  };
 }
