22ec6d4add54b2a906b20b05fc82d68418571bc1
CN010627 <guanxing.zhu@sdlg.com>
2025-11-06 08:57:17 +0800

    u

--- system/greet.nix
+++ system/greet.nix
@@ -9,14 +9,14 @@ let
 in
 {
   environment.systemPackages = with pkgs; [
-    tuigreet
+    greetd.tuigreet
   ];
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         user = "${username}";
-        command = "${pkgs.tuigreet}/bin/tuigreet -g 'Welcome to the LeviLumos!' --user-menu --time --time-format '%A, %B %d, %Y - %I:%M:%S %p' --asterisks --greet-align center --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"; # start Hyprland with a TUI login manager
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g 'Welcome to the LeviLumos!' --user-menu --time --time-format '%A, %B %d, %Y - %I:%M:%S %p' --asterisks --greet-align center --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"; # start Hyprland with a TUI login manager
       };
       # 自动登录: --cmd ${session}
       initial_session = {
