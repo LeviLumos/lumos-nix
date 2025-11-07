2399dcfb5cf20c5777495492f49b9c030725464c
CN010627 <guanxing.zhu@sdlg.com>
2025-11-07 13:25:35 +0800

    u

--- home/default.nix
+++ home/default.nix
@@ -1,16 +1,10 @@
 {
   imports = [
-    # ./fuzzel.nix
-    # ./ghostty.nix
-    # ./hyprpanel
-    # ./hypr
-    # ./git.nix
-    # ./nvf.nix
-    # ./qutebrowser
-    # ./zen
-    # ./cava.nix
-    # ./btm.nix
-    # ./btop.nix
-    # ./lazygit.nix
+    ./ghostty.nix
+    ./git.nix
+    ./nvf.nix
+    ./zen
+    ./btop.nix
+    ./lazygit.nix
   ];
 }
